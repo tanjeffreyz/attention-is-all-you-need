98f49a5d34ece1fc7ca3015590ccaa90a4e2d307
tanjeffreyz <tanjeffreyz02@berkeley.edu>
2023-08-16 15:41:38 -0700

    dimension issues

--- train.py
+++ train.py
@@ -8,13 +8,19 @@ dataset = Dataset(config.LANGUAGE_PAIR, batch_size=config.BATCH_SIZE)
 
 # Initialize model with same seed every time
 torch.manual_seed(config.SEED)
-model = Transformer(config.D_MODEL, len(dataset.src_vocab), len(dataset.trg_vocab))
+model = Transformer(
+    config.D_MODEL,
+    len(dataset.src_vocab),
+    len(dataset.trg_vocab)
+)
 torch.seed()        # Reseed afterward b/c want shuffled data
 
-print(next(iter(dataset.train_loader)))
+# print(next(iter(dataset.train_loader)))
 test = next(iter(dataset.train_loader))
 src = test['source']
 trg = test['target']
 print(src.size())
 print(' '.join(dataset.src_vocab.lookup_tokens(list(src[0]))))
 print(' '.join(dataset.trg_vocab.lookup_tokens(list(trg[0]))))
+
+print(model(src, trg).size(), len(dataset.trg_vocab))
