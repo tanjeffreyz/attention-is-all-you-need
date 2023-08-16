b5da9372816a1f84c9e87ad6b7521b3ce5260c87
tanjeffreyz <tanjeffreyz02@berkeley.edu>
2023-08-16 00:48:57 -0700

    added positional encoding

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import config
+import torch
 from torch import nn
 
 
@@ -8,5 +9,15 @@ class Embedding(nn.Module):
 
         self.embedding = nn.Embedding(len(vocab), config.D_MODEL)
 
+    def positional_encoding(self, x):
+        result = torch.zeros((x.size(1), config.D_MODEL), dtype=torch.float)
+        pos = torch.arange(0, x.size(1)).unsqueeze(1)
+        dim = torch.arange(0, config.D_MODEL)
+
+        # Sine for even positions, cosine for odd positions
+        result[:, 0::2] = torch.sin(pos / (10_000 ** (dim[0::2] / config.D_MODEL)))
+        result[:, 1::2] = torch.cos(pos / (10_000 ** (dim[1::2] / config.D_MODEL)))
+        return result
+
     def forward(self, x):
-        return self.embedding(x)
+        return self.embedding(x) + self.positional_encoding(x)
