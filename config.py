b7148497de169b25a3c50a3a6cab40a9ef6036f9
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-08-16 21:25:20 -0700

    ready to write trianing script

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
+# Data parameters
 LANGUAGE_PAIR = ('en', 'de')
-
 D_MODEL = 512
 
+# Training parameters
 BATCH_SIZE = 64
-
 LEARNING_RATE = 1E-4
-
-NUM_ITERATIONS = 10_000
+NUM_EPOCHS = 1_000
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import torch
 import config
 from data import Dataset
 from models import Transformer
@@ -5,21 +6,15 @@ from models import Transformer
 
 print(f'[~] Using device: {Transformer.device}')
 
-# Load data
 dataset = Dataset(config.LANGUAGE_PAIR, batch_size=config.BATCH_SIZE)
 
-# Initialize model with same seed every time to keep embeddings consistent
 model = Transformer(
     config.D_MODEL,
     len(dataset.src_vocab),
     len(dataset.trg_vocab)
 )
 
-test = next(iter(dataset.train_loader))
-src = test['source'].to(model.device)
-trg = test['target'].to(model.device)
-index = min(src.size(-1), trg.size(-1)) - 1
-print('IN: ', src.size(), trg.size())
-print(' '.join(dataset.src_vocab.lookup_tokens(list(src[index]))))
-print(' '.join(dataset.trg_vocab.lookup_tokens(list(trg[index]))))
-print('OUT:', model(src, trg).size(), len(dataset.trg_vocab))
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=config.LEARNING_RATE
+)
