83959570e771fc296896488336263d01b6cb94e5
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-08-17 21:08:20 -0700

    cleanup

--- models/embedding.py
+++ models/embedding.py
@@ -19,7 +19,6 @@ class Embedding(Module):
     def positional_encoding(self, x):
         # result.shape = (seq_len, d_model)
         result = torch.zeros((x.size(1), self.d_model), dtype=torch.float)
-        result.requires_grad = False
 
         # pos.shape = (seq_len, 1)
         pos = torch.arange(0, x.size(1)).unsqueeze(1)
