f2fdba26579dd27b3e0bd08c8a789b061fd7a9ea
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-08-17 20:05:35 -0700

    created separate dropout layers for clarity

--- models/decoder.py
+++ models/decoder.py
@@ -10,25 +10,26 @@ class DecoderLayer(Module):
 
         self.masked_attention = MultiHeadAttention(d_model, num_heads=num_heads, use_mask=True)
         self.layer_norm1 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(p=dropout_rate)
 
         self.enc_attention = MultiHeadAttention(d_model, num_heads=num_heads)
         self.layer_norm2 = nn.LayerNorm(d_model)
+        self.dropout2 = nn.Dropout(p=dropout_rate)
 
         self.ffn = FeedForwardNetwork(d_model)
         self.layer_norm3 = nn.LayerNorm(d_model)
-
-        self.dropout = nn.Dropout(p=dropout_rate)
+        self.dropout3 = nn.Dropout(p=dropout_rate)
 
     def forward(self, x, enc_out):
         # Multi-headed attention and residual connection + layer norm
         attention_out = self.masked_attention(queries=x, keys=x, values=x)
-        x = self.layer_norm1(x + self.dropout(attention_out))
+        x = self.layer_norm1(x + self.dropout1(attention_out))
 
         # Multi-headed attention over output of encoder stack
         # Use ENC_OUT as the keys and values, the queries come from previous attention
         attention_out = self.enc_attention(queries=x, keys=enc_out, values=enc_out)
-        x = self.layer_norm2(x + self.dropout(attention_out))
+        x = self.layer_norm2(x + self.dropout2(attention_out))
 
         # Feed-forward network and another residual + layer norm
         ffn_out = self.ffn(x)
-        return self.layer_norm3(x + self.dropout(ffn_out))
+        return self.layer_norm3(x + self.dropout3(ffn_out))
--- models/encoder.py
+++ models/encoder.py
@@ -10,18 +10,18 @@ class EncoderLayer(Module):
 
         self.self_attention = MultiHeadAttention(d_model, num_heads=num_heads)
         self.layer_norm1 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(p=dropout_rate)
 
         self.ffn = FeedForwardNetwork(d_model)
         self.layer_norm2 = nn.LayerNorm(d_model)
-
-        self.dropout = nn.Dropout(p=dropout_rate)
+        self.dropout2 = nn.Dropout(p=dropout_rate)
 
     def forward(self, x):
         # Multi-headed attention and residual connection + layer norm
         # Dropout is applied to sub-layer output, before residual and norm
         attention_out = self.self_attention(queries=x, keys=x, values=x)
-        x = self.layer_norm1(x + self.dropout(attention_out))
+        x = self.layer_norm1(x + self.dropout1(attention_out))
 
         # Feed-forward network and another residual + layer norm
         ffn_out = self.ffn(x)
-        return self.layer_norm2(x + self.dropout(ffn_out))
+        return self.layer_norm2(x + self.dropout2(ffn_out))
