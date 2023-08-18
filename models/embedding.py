2b21d79dda7bc9331de570d72bfa036b38757d34
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-08-18 12:45:38 -0700

    fixed positional encoding

--- models/embedding.py
+++ models/embedding.py
@@ -28,9 +28,9 @@ class Embedding(Module):
         pos = torch.arange(0, x.size(1)).unsqueeze(1)
 
         # dim.shape = (d_model)
-        dim = torch.arange(0, self.d_model)
+        dim = torch.arange(0, self.d_model, step=2)
 
         # Sine for even positions, cosine for odd dimensions
-        result[:, 0::2] = torch.sin(pos / (10_000 ** (dim[0::2] / self.d_model)))
-        result[:, 1::2] = torch.cos(pos / (10_000 ** (dim[1::2] / self.d_model)))
+        result[:, 0::2] = torch.sin(pos / (10_000 ** (dim / self.d_model)))
+        result[:, 1::2] = torch.cos(pos / (10_000 ** (dim / self.d_model)))
         return result.to(self.device)
